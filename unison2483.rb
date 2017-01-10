21d08b3928f83ff6b4d12cb1335bc3a3aae3cd86
Kailey Lampert <trepmal@gmail.com>
2017-01-09 23:00:15 -0800

    Update unison2483.rb

--- unison2483.rb
+++ unison2483.rb
@@ -13,9 +13,9 @@ class Unison2483 < Formula
     
     # Remove unrecognized options if warned by configure
     #system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    #                      "--disable-dependency-tracking",
+    #                      "--disable-silent-rules",
+    #                      "--prefix=#{prefix}"
     # system "cmake", ".", *std_cmake_args
 #    system "make", "install" # if this fails, try separate make/make install steps
     system "make", "install" # if this fails, try separate make/make install steps
